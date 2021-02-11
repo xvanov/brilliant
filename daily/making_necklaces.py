3ca9af4281c261d29c222f15cfc2071820f24d5a
xvanov <xvanov@gmail.com>
2021-02-11 06:17:35 -0800

    correctly solve making necklaces

--- daily/making_necklaces.py
+++ daily/making_necklaces.py
@@ -7,25 +7,27 @@ if __name__ == '__main__':
     total = 2**N # total combinations
     matched = [] # list of matched binary numbers, so I don't go over them again
 
-    binary_nums = [format(i, '#08b')[2:] for i in range(0, total)]
-    
-
-
+    #binary_nums = [format(i, '#08b')[2:] for i in range(0, total)]
+    c = 0
+    new = []
     for i in range(0, total):
-        if i not in matched:
-            b = format(i, '#08b')[2:]
-            for f in range(1): # flip
-                if f == 1:
-                    b = b[::-1]
-                tmp = b
-                for j in range(len(b)): # rotate around                
-                    tmp = tmp[1:]+tmp[0]
-                    tmp_b = int('0b'+tmp, 2)
-                    if tmp_b not in matched:
-                        matched.append(tmp_b)
-        else:
-            print(i)
-            matched.append(i)
-    unique_matched = set(matched)
-    print(matched)
-    print(len(unique_matched))
+        b = format(i, '#08b')[2:]
+        generator_bool = False
+        for f in range(2): # flip
+            if f == 1:
+                b = b[::-1]
+            tmp = b
+            for j in range(len(b)): # rotate around                
+                tmp = tmp[1:]+tmp[0]
+                tmp_n = int('0b'+tmp, 2)
+                if tmp not in matched:
+                    print(i,b,tmp)
+                    matched.append(tmp)
+                    generator_bool = True
+
+        if generator_bool:
+            c+=1
+            new.append((i,b))
+    print(total)
+    print(c)
+    print(new)
