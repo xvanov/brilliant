covariance correct problem

quant/covariance.py
@@ -71,7 +71,8 @@ print(mycov)
 cov_ab = cov_sum(X2,X)
 print('sum of x2 and x vars', cov_ab)
 
-N = 1000
+N = 100000
+
 stks = []
 for i in range(N):
     c = random.choice([1,2,3])
@@ -84,3 +85,6 @@ D = [p[1] for p in stks]
 stk_cov = np.cov(np.array(U), np.array(D), bias=True)
 
 print(stk_cov)
+
+
+