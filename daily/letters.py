00b597ca96ef5acce87dac0dc82fe4b17fbbdff6
Ubuntu <xvanov@gmail.com>
2021-04-03 13:52:15 +0000

    c: letters: multiply two words

--- daily/letters.py
+++ daily/letters.py
@@ -21,7 +21,33 @@ def mul_of_letters(w1, w2, r):
                         print(n2)
                         print(r)
 
+def mul_2_digits(w1, w2, r):
+    r = 0
+    T = 10
+    for a in range(T):
+        for b in range(T):
+            n1 = a*10+b
+            n2 = a*10+b
+            r = n1*n2
+            tmp = str(r)
+
+            # condition
+            if len(tmp) >= 4:
+                raise Exception(f'result {r} is too big!')
+            if len(tmp) < 3:
+               continue
+
+            d1 = int(tmp[0])
+            d2 = int(tmp[1])
+            d3 = int(tmp[2])
+            if d2 == a and d3 == b:           
+                print(n1,n2,r)
+                break
+        else:
+            continue
+        break
 
 
 
 if __name__ == '__main__':
+    mul_2_digits(0,0,0)
